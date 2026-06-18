536825efe2ab80f30d40d29d9ea666d4c3cff7ed
poosmacker <103903987+PooSmacker@users.noreply.github.com>
2026-06-18 22:35:11 +0100

    fix #24

--- src/main/java/com/dripps/voxyserver/network/VoxyServerNetworking.java
+++ src/main/java/com/dripps/voxyserver/network/VoxyServerNetworking.java
@@ -14,7 +14,6 @@ public class VoxyServerNetworking {
         PayloadTypeRegistry.clientboundPlay().register(LODServerSettingsPayload.TYPE, LODServerSettingsPayload.CODEC);
         PayloadTypeRegistry.clientboundPlay().register(LODProtocolPayload.TYPE, LODProtocolPayload.CODEC);
 
-        PayloadTypeRegistry.serverboundPlay().register(LODReadyPayload.TYPE, LODReadyPayload.CODEC);
         PayloadTypeRegistry.serverboundPlay().register(LODPreferencesPayload.TYPE, LODPreferencesPayload.CODEC);
         PayloadTypeRegistry.serverboundPlay().register(LODManifestPayload.TYPE, LODManifestPayload.CODEC);
         PayloadTypeRegistry.serverboundPlay().register(LODHandshakePayload.TYPE, LODHandshakePayload.CODEC);
